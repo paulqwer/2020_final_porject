5f8d826d70898f29c9469b3c68b74801129f351b
paul <lin880903roy89@gmail.com>
2020-12-25 03:55:00 +0800

    add choose state

--- fsm.py
+++ fsm.py
@@ -100,39 +100,40 @@ class TocMachine(GraphMachine):
                 break
             else :
                 index += 1
-        send_text_message(event.reply_token,times)
 
     def on_enter_male2(self,event):
-        num1 = random.randint(0,15)
-        num2 = random.randint(0,15)
-        while male_used[num1] != 0 or male_used[num1] != -1:
-            num1 = random.randint(0,15)
-        while num1 == num2 or male_used[num2] != 1 or male_used[num2] != -1:
-            num2 = random.randint(0,15)
-        male_used[num1] = -1
-        male_used[num2] = -1
-        s1 = male_twicher_name[num1]
-        s2 = male_twicher_name[num2]
-        line_bot_api.reply_message(
-            event.reply_token,
-            TemplateSendMessage(
-                alt_text = 'Button template',
-                template = ButtonsTemplate(
-                    title = '選擇',
-                    text = '請選擇你最喜歡的實況主',
-                    actions = [
-                        MessageTemplateAction(
-                            label = s1,
-                            text = s1
-                        ),
-                        MessageTemplateAction(
-                            label = s2,
-                            text = s2
-                        )
-                    ]
-                )
-            )
-        )
+        reply_token = event.reply_token
+        send_text_message(reply_token, "nothing")
+        # num1 = random.randint(0,15)
+        # num2 = random.randint(0,15)
+        # while male_used[num1] != 0 or male_used[num1] != -1:
+        #     num1 = random.randint(0,15)
+        # while num1 == num2 or male_used[num2] != 1 or male_used[num2] != -1:
+        #     num2 = random.randint(0,15)
+        # male_used[num1] = -1
+        # male_used[num2] = -1
+        # s1 = male_twicher_name[num1]
+        # s2 = male_twicher_name[num2]
+        # line_bot_api.reply_message(
+        #     event.reply_token,
+        #     TemplateSendMessage(
+        #         alt_text = 'Button template',
+        #         template = ButtonsTemplate(
+        #             title = '選擇',
+        #             text = '請選擇你最喜歡的實況主',
+        #             actions = [
+        #                 MessageTemplateAction(
+        #                     label = s1,
+        #                     text = s1
+        #                 ),
+        #                 MessageTemplateAction(
+        #                     label = s2,
+        #                     text = s2
+        #                 )
+        #             ]
+        #         )
+        #     )
+        # )
     def on_enter_male3(self,event):
         num1 = random.randint(0,15)
         num2 = random.randint(0,15)
