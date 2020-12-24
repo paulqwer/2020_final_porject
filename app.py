aa597f68e80a5e10a121ab5087864f9eb07c0806
paul <lin880903roy89@gmail.com>
2020-12-24 23:24:37 +0800

    add choose state

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def webhook_handler():
                 machine.introduce(event)
             if event.message.text == "開始":
                 machine.to_choose(event)
-        if machine.state == "male_1_1":
+        if machine.state == "choose":
             if event.message.text == "男性":
                 machine.to_male_1_1(event)
         elif machine.state == "to_male_1":
