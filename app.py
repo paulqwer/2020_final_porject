9cce231ff548b5871f51cd386704bff0326b7ba1
paul <lin880903roy89@gmail.com>
2020-12-26 04:02:43 +0800

    add

--- app.py
+++ app.py
@@ -200,7 +200,15 @@ def webhook_handler():
                     machine.fdo_female_comp2(event,ft2,xx)
         elif machine.state == "female3":
             if event.message.text == "Mita" or event.message.text == "小熊" or event.message.text == "湘湘" or event.message.text == "凱琪" or event.message.text == "愷蒂喵" or event.message.text == "妮妮" or event.message.text == "企鵝妹" or event.message.text == "ViVi" or event.message.text == "蛋捲" or event.message.text == "優格" or event.message.text == "小雲寶寶" or event.message.text == "諾曼" or event.message.text == "妮婭" or event.message.text =="劉萱" or event.message.text == "阿樂" or event.message.text == "天菜娘娘":
-                machine.do_nothing(event)
+                ft3 = event.message.text 
+                ff_3_times += 1
+                x3 = ff_3_times
+                if ff_3_times == 2:
+                    machine.fdo_something_ver3(event,ft3)
+                    # machine.do_nothing(event)
+                    machine.to_female4(event)
+                else :
+                    machine.fdo_female_comp3(event,ft3,x3)
         # if machine.state == "male2":    #t = 2
         #     if  event.message.text == "6tan" or event.message.text == "餐哥" or event.message.text == "鳥屎" or event.message.text == "國棟" or event.message.text == "虧皮" or event.message.text == "館長" or event.message.text == "爆哥" or event.message.text == "Rex" or event.message.text == "KO" or event.message.text == "Toyz" or event.message.text == "NL(MK)" or event.message.text == "老皮" or event.message.text == "史丹利" or event.message.text =="花輪" or event.message.text == "懶貓" or event.message.text == "UZRA":
         #         index = 0
--- fsm.py
+++ fsm.py
@@ -434,6 +434,46 @@ class TocMachine(GraphMachine):
                 )
             )
         )
+    def fdo_female_comp3(self,event,t3,x3):
+        global female_uese
+        ind = 0
+        while ind < 16:
+            if female_twitcher_name[ind] == t3:
+                female_uese[ind] = 4
+                break
+            else :
+                ind += 1
+        num1 = random.randint(0,15)
+        num2 = random.randint(0,15)
+        while female_uese[num1] == -1 or female_uese[num1] == 4:
+            num1 = random.randint(0,15)
+        while num1 == num2  or female_uese[num2] == -1 or female_uese[num2] == 4:
+            num2 = random.randint(0,15)
+        female_uese[num1] = -1
+        female_uese[num2] = -1
+        s1 = female_twitcher_name[num1]
+        s2 = female_twitcher_name[num2]
+        line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(
+                alt_text = 'Button template',
+                template = ButtonsTemplate(
+                    title = str(x3+1) + "/2選擇",
+                    text = '請選擇你最喜歡的實況主',
+                    actions = [
+                        MessageTemplateAction(
+                            label = s1,
+                            text = s1
+                        ),
+                        MessageTemplateAction(
+                            label = s2,
+                            text = s2
+                        )
+                    ]
+                )
+            )
+        )
+
     def on_enter_female(self,event):
         global female_uese
         num1 = random.randint(0,15)
@@ -518,7 +558,7 @@ class TocMachine(GraphMachine):
             TemplateSendMessage(
                 alt_text = 'Button template',
                 template = ButtonsTemplate(
-                    title = '1/4選擇',
+                    title = '1/2選擇',
                     text = '請選擇你最喜歡的實況主',
                     actions = [
                         MessageTemplateAction(
