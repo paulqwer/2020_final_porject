96bd420cbef8769f5ebe8e4107c4aae7de511b4b
paul <lin880903roy89@gmail.com>
2020-12-25 16:04:47 +0800

    add

--- app.py
+++ app.py
@@ -129,13 +129,13 @@ def webhook_handler():
                     machine.do_male1_comp(event,te,x)
         if machine.state == "male2": 
             if  event.message.text == "6tan" or event.message.text == "餐哥" or event.message.text == "鳥屎" or event.message.text == "國棟" or event.message.text == "虧皮" or event.message.text == "館長" or event.message.text == "爆哥" or event.message.text == "Rex" or event.message.text == "KO" or event.message.text == "Toyz" or event.message.text == "NL(MK)" or event.message.text == "老皮" or event.message.text == "史丹利" or event.message.text =="花輪" or event.message.text == "懶貓" or event.message.text == "UZRA":
-                tt = event.message.text
-                f_1_times += 1 
+                tt = event.message.text 
                 x2 = f_1_times
                 if f_1_times == 4:
                     machine.do_nothing(event)
                 else :
                     machine.do_male2_comp(event,tt,x2)
+                    f_1_times += 1
         # if machine.state == "male2":    #t = 2
         #     if  event.message.text == "6tan" or event.message.text == "餐哥" or event.message.text == "鳥屎" or event.message.text == "國棟" or event.message.text == "虧皮" or event.message.text == "館長" or event.message.text == "爆哥" or event.message.text == "Rex" or event.message.text == "KO" or event.message.text == "Toyz" or event.message.text == "NL(MK)" or event.message.text == "老皮" or event.message.text == "史丹利" or event.message.text =="花輪" or event.message.text == "懶貓" or event.message.text == "UZRA":
         #         index = 0
