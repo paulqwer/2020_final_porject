e0e883a8ce49394c73f9bd6a1029772674b51b4c
paul <lin880903roy89@gmail.com>
2020-12-25 17:49:16 +0800

    add

--- app.py
+++ app.py
@@ -134,8 +134,16 @@ def webhook_handler():
             if  event.message.text == "6tan" or event.message.text == "餐哥" or event.message.text == "鳥屎" or event.message.text == "國棟" or event.message.text == "虧皮" or event.message.text == "館長" or event.message.text == "爆哥" or event.message.text == "Rex" or event.message.text == "KO" or event.message.text == "Toyz" or event.message.text == "NL(MK)" or event.message.text == "老皮" or event.message.text == "史丹利" or event.message.text =="花輪" or event.message.text == "懶貓" or event.message.text == "UZRA":
                 # machine.do_print(event)
                 tt = event.message.text 
-                global f_1_times
-                f_1_times += 1
+                if f_1_times == 0:
+                    f_1_times += 1
+                elif f_1_times == 1:
+                    f_1_times += 1
+                elif f_1_times == 2:
+                    f_1_times += 1
+                elif f_1_times == 3:
+                    f_1_times += 1
+                else:
+                    pass
                 if f_1_times == 4:
                     machine.do_nothing(event)
                 else :
