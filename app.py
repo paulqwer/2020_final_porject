8955703167297db5dc1a36b5dafe034004ae4691
paul <lin880903roy89@gmail.com>
2020-12-25 12:33:02 +0800

    add

--- app.py
+++ app.py
@@ -121,9 +121,10 @@ def webhook_handler():
                 te = event.message.text
                 global f_1_times
                 f_1_times = f_1_times +1
-                machine.do_male1_comp(event,te)
-                if f_1_times == 8:
+                if f_1_times == 7:
                     machine.do_nothing(event)
+                else :
+                    machine.do_male1_comp(event,te)
         # if machine.state == "male2":    #t = 2
         #     if  event.message.text == "6tan" or event.message.text == "餐哥" or event.message.text == "鳥屎" or event.message.text == "國棟" or event.message.text == "虧皮" or event.message.text == "館長" or event.message.text == "爆哥" or event.message.text == "Rex" or event.message.text == "KO" or event.message.text == "Toyz" or event.message.text == "NL(MK)" or event.message.text == "老皮" or event.message.text == "史丹利" or event.message.text =="花輪" or event.message.text == "懶貓" or event.message.text == "UZRA":
         #         index = 0
