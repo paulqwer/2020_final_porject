e78e2127d9052bd9a1b9291524ea95956a942627
paul <lin880903roy89@gmail.com>
2020-12-25 14:42:33 +0800

    add

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def webhook_handler():
             continue
         print(f"\nFSM STATE: {machine.state}")
         print(f"REQUEST BODY: \n{body}")
-
+        global f_1_times
         if event.message.text == "結束":
             machine.go_back_intro(event)
         if machine.state == "in":
@@ -120,7 +120,6 @@ def webhook_handler():
         if machine.state == "male":     #t = 1
             if  event.message.text == "6tan" or event.message.text == "餐哥" or event.message.text == "鳥屎" or event.message.text == "國棟" or event.message.text == "虧皮" or event.message.text == "館長" or event.message.text == "爆哥" or event.message.text == "Rex" or event.message.text == "KO" or event.message.text == "Toyz" or event.message.text == "NL(MK)" or event.message.text == "老皮" or event.message.text == "史丹利" or event.message.text =="花輪" or event.message.text == "懶貓" or event.message.text == "UZRA":
                 te = event.message.text
-                global f_1_times
                 f_1_times = f_1_times +1
                 x = f_1_times
                 if f_1_times == 8:
@@ -128,7 +127,7 @@ def webhook_handler():
                     machine.to_male2(event)
                 else :
                     machine.do_male1_comp(event,te,x)
-        if machine.state == "male2": #t = 2
+        if machine.state == "male2": 
             if  event.message.text == "6tan" or event.message.text == "餐哥" or event.message.text == "鳥屎" or event.message.text == "國棟" or event.message.text == "虧皮" or event.message.text == "館長" or event.message.text == "爆哥" or event.message.text == "Rex" or event.message.text == "KO" or event.message.text == "Toyz" or event.message.text == "NL(MK)" or event.message.text == "老皮" or event.message.text == "史丹利" or event.message.text =="花輪" or event.message.text == "懶貓" or event.message.text == "UZRA":
                 tt = event.message.text
                 f_1_times += 1 
