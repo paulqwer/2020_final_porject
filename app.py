d05b95dc39f2b1ee3f500ee948470b1b5cb82361
paul <lin880903roy89@gmail.com>
2020-12-25 18:20:41 +0800

    add

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ male_twicher_name = ['6tan','餐哥','鳥屎','國棟','虧皮','館長','爆哥
 
 f_1_times = 0
 f_2_times = 0
-
+f_3_times = 0
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
@@ -96,7 +96,7 @@ def webhook_handler():
     except InvalidSignatureError:
         abort(400)
 
-    global f_1_times,f_2_times
+    global f_1_times,f_2_times,f_3_times
     # if event is MessageEvent and message is TextMessage, then echo text
     for event in events:
         if not isinstance(event, MessageEvent):
@@ -143,6 +143,16 @@ def webhook_handler():
                 else :
                     machine.do_male2_comp(event,tt,xx)
         elif machine.state == "male3":
+            if  event.message.text == "6tan" or event.message.text == "餐哥" or event.message.text == "鳥屎" or event.message.text == "國棟" or event.message.text == "虧皮" or event.message.text == "館長" or event.message.text == "爆哥" or event.message.text == "Rex" or event.message.text == "KO" or event.message.text == "Toyz" or event.message.text == "NL(MK)" or event.message.text == "老皮" or event.message.text == "史丹利" or event.message.text =="花輪" or event.message.text == "懶貓" or event.message.text == "UZRA":
+                t3 = event.message.text 
+                f_3_times += 1
+                x3 = f_3_times
+                if f_3_times == 2:
+                    machine.do_something_ver2(event,t3)
+                    machine.to_male4(event)
+                else :
+                    machine.do_male3_comp(event,t3,x3)
+        elif machine.state == "male4":
             if  event.message.text == "6tan" or event.message.text == "餐哥" or event.message.text == "鳥屎" or event.message.text == "國棟" or event.message.text == "虧皮" or event.message.text == "館長" or event.message.text == "爆哥" or event.message.text == "Rex" or event.message.text == "KO" or event.message.text == "Toyz" or event.message.text == "NL(MK)" or event.message.text == "老皮" or event.message.text == "史丹利" or event.message.text =="花輪" or event.message.text == "懶貓" or event.message.text == "UZRA":
                 machine.do_nothing(event)
 
--- fsm.py
+++ fsm.py
@@ -220,6 +220,8 @@ class TocMachine(GraphMachine):
                 break
             else :
                 ind += 1
+    
+
     def do_initia(self,event):
         index = 0
         while index <16:
@@ -233,7 +235,7 @@ class TocMachine(GraphMachine):
             index += 1
         reply_token = event.reply_token
         send_text_message(reply_token,s)
-        
+
     def on_enter_male3(self,event):
         num1 = random.randint(0,15)
         num2 = random.randint(0,15)
@@ -265,5 +267,43 @@ class TocMachine(GraphMachine):
                 )
             )
         )
-    
+
+    def do_male3_comp(self,event,t3,x3):
+        ind = 0
+        while ind < 16:
+            if male_twicher_name[ind] == t3:
+                male_used[ind] = 4
+                break
+            else :
+                ind += 1
+        num1 = random.randint(0,15)
+        num2 = random.randint(0,15)
+        while male_used[num1] == -1 or male_used[num1] == 4:
+            num1 = random.randint(0,15)
+        while num1 == num2  or male_used[num2] == -1 or male_used[num2] == 4:
+            num2 = random.randint(0,15)
+        male_used[num1] = -1
+        male_used[num2] = -1
+        s1 = male_twicher_name[num1]
+        s2 = male_twicher_name[num2]
+        line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(
+                alt_text = 'Button template',
+                template = ButtonsTemplate(
+                    title = str(x3+1) + "/2選擇",
+                    text = '請選擇你最喜歡的實況主',
+                    actions = [
+                        MessageTemplateAction(
+                            label = s1,
+                            text = s1
+                        ),
+                        MessageTemplateAction(
+                            label = s2,
+                            text = s2
+                        )
+                    ]
+                )
+            )
+        )
        
